0e261f4ef0f624971b8f34afa51885f77e8b7b14
Alejandro Ramírez <alejandro.ramirez@pjpuebla.gob.mx>
2024-05-22 13:24:56 -0600

    View para el reporte MediacionSolicitud

--- sql/actualizaciones.sql
+++ sql/actualizaciones.sql
@@ -21,3 +21,25 @@ $function$
 ;
 
 create sequence mediacion.seq_foliador start with 1;
+
+-- mediacion.qry_solicitud source
+
+CREATE OR REPLACE VIEW mediacion.qry_solicitud
+AS SELECT s.id,
+    s.folio,
+    s.fecha_solicitud,
+    m.descripcion AS materia,
+    p.nombre AS usuario_nombre,
+    p.apellido_paterno AS usuario_apaterno,
+    p.apellido_materno AS usuario_amaterno,
+    p2.nombre AS invitado_nombre,
+    p2.apellido_paterno AS invitado_apaterno,
+    p2.apellido_materno AS invitado_amaterno,
+    s.descripcion_conflicto
+   FROM mediacion.solicitud s
+     JOIN core.persona p ON p.id = s.usuario_persona_id
+     JOIN core.persona p2 ON p2.id = s.invitado_persona_id
+     JOIN core.materia m ON m.id = s.materia_id
+     JOIN mediacion.tipo_apertura ta ON ta.id = s.tipo_apertura_id;
+    
+GRANT SELECT ON TABLE mediacion.qry_solicitud TO pjpuebla;
