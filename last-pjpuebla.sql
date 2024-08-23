bfb71fc770e6c67beddeda586bb271f051f61598
alex <2013rex@gmail.com>
2024-08-23 10:59:40 -0600

    actualización de BD

--- last-pjpuebla.sql
+++ last-pjpuebla.sql
@@ -15,7 +15,7 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
+mediador
 --
 -- Name: core; Type: SCHEMA; Schema: -; Owner: pjpuebla
 --
